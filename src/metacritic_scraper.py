7aea4cb603e9c0beb5405e8bed8b2bf32edc04db
Joe Alsko <joal2716@colorado.edu>
2018-08-25 18:58:11 -0600

    Finished implementing basic movie credits tables (movie_people and credits tables for each movie)

--- src/metacritic_scraper.py
+++ src/metacritic_scraper.py
@@ -6,22 +6,25 @@ import sys
 import re
 
 def getPersonScore(person_name):
-	adjustedName = (person_name.replace(' ', '-').replace('.', '')).lower()
+	adjustedName = (person_name.replace(' ', '-').replace('.', '')).lower().encode('ascii', 'ignore').decode('ascii')
 	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 	search_url = 'http://www.metacritic.com/person/' + adjustedName
-	headers= {'User-Agent':user_agent} 
-	request=urllib.request.Request(search_url,None,headers) #The assembled request
+	headers = {'User-Agent':user_agent} 
+	request = urllib.request.Request(search_url,None,headers) #The assembled request
 	#response = urllib.request.urlopen(request)
 
 	try:
 	    response = urllib.request.urlopen(request)
 	except urllib.error.HTTPError as e:
 	    #if e.code == 404:
-	    print ('Person not found!')
+	    print (person_name + ' not found!')
 	    return -1
 
 	search_html = response.read()
 	search_soup = BeautifulSoup(search_html, "html.parser")
 	td_list = search_soup.findAll('td', {'class':'summary_score'})
-	print (td_list[0].get_text().replace('\n', ''))
+	if (len(td_list) <= 0):
+		print (person_name + ' not found!')
+		return -1
+	#print (td_list[0].get_text().replace('\n', ''))
 	return int(td_list[0].get_text().replace('\n', ''))
